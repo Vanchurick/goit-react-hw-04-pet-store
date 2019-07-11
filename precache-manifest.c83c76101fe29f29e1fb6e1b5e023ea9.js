5e7ef509ab53057392d6f06fb6ee138b6583d23b
Vanchurick <davane4ek@gmail.com>
2019-07-11 18:00:04 +0300

    Updates

---
+++ precache-manifest.c83c76101fe29f29e1fb6e1b5e023ea9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee81ba2fed99fa32d1cad7264867ec44",
+    "url": "/goit-react-hw-04-pet-store/index.html"
+  },
+  {
+    "revision": "498e039839f6650793da",
+    "url": "/goit-react-hw-04-pet-store/static/css/main.47c78589.chunk.css"
+  },
+  {
+    "revision": "9b23702e82d8791b6a00",
+    "url": "/goit-react-hw-04-pet-store/static/js/2.af0af5c1.chunk.js"
+  },
+  {
+    "revision": "498e039839f6650793da",
+    "url": "/goit-react-hw-04-pet-store/static/js/main.b5178385.chunk.js"
+  },
+  {
+    "revision": "b0947ce383148671feb4",
+    "url": "/goit-react-hw-04-pet-store/static/js/runtime~main.081fabb4.js"
+  }
+]);
