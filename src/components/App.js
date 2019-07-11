44945c1f8cc4e84821497ebc55848f7f4c490057
Vanchurick <davane4ek@gmail.com>
2019-07-11 17:55:18 +0300

    hw4 pets

---
+++ src/components/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import './App.css';
+import Navigation from './Navigation/Navigation';
+import HomePage from './pages/HomePage/HomePage';
+import AboutPage from './pages/AboutPage/AboutPage';
+import PetPage from './pages/PetPage/PetPage';
+import PetsPage from './pages/PetsPage/PetsPage';
+import PageNotFound from './pages/PageNotFound/PageNotFound';
+
+export default class App extends Component {
+  state = {};
+
+  render() {
+    return (
+      <div>
+        <Navigation />
+        <Switch>
+          <Route path="/" exact component={HomePage} />
+          <Route path="/about" component={AboutPage} />
+          <Route path="/pets/:id" component={PetPage} />
+          <Route path="/pets" component={PetsPage} />
+          <Route component={PageNotFound} />
+        </Switch>
+      </div>
+    );
+  }
+}
