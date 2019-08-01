7fd0b358f19a6d555ba5dc933983ddd8397fa247
Vanchurick <davane4ek@gmail.com>
2019-08-01 17:32:52 +0300

    Updates

---
+++ precache-manifest.b766aec46a9ce0919b48f4e379180ba0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf06bdfd081dea3567a15a50e030d48a",
+    "url": "/goit-react-hw-04-pet-store/index.html"
+  },
+  {
+    "revision": "81297f59562baf279043",
+    "url": "/goit-react-hw-04-pet-store/static/css/main.6a0f66de.chunk.css"
+  },
+  {
+    "revision": "0fe93fcf797641eaa066",
+    "url": "/goit-react-hw-04-pet-store/static/js/2.12cbc5cd.chunk.js"
+  },
+  {
+    "revision": "81297f59562baf279043",
+    "url": "/goit-react-hw-04-pet-store/static/js/main.c55e34a8.chunk.js"
+  },
+  {
+    "revision": "b0947ce383148671feb4",
+    "url": "/goit-react-hw-04-pet-store/static/js/runtime~main.081fabb4.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/goit-react-hw-04-pet-store/precache-manifest.03e515b7064efa4f45852214ab045313.js"
+  "/goit-react-hw-04-pet-store/precache-manifest.b766aec46a9ce0919b48f4e379180ba0.js"
 );
 
 self.addEventListener('message', (event) => {
